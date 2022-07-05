972fe823d4a3de354e248e02b517effe9819021a
StiLKnoT <stilknot@gmail.com>
2022-07-05 21:10:44 +0500

    sssa

--- src/App.js
+++ src/App.js
@@ -15,16 +15,16 @@ import TabMenu from './components/TabMenu/TabMenu'
 function App() {
   return (
     <div className="App">
-      {/* <Navbar /> */}
-      {/* <MainTop /> */}
-      {/* <KouchDasturi /> */}
+      <Navbar />
+      <MainTop />
+      <KouchDasturi />
       <CarouselCards />
-      {/* <VideosBlock /> */}
-      {/* <KouchUstunligi /> */}
+      <VideosBlock />
+      <KouchUstunligi />
       <KouchAccor />
       <CarouselVideos />
       <LogoSection />
-      {/* <WhoBlock /> */}
+      <WhoBlock />
       <FAQs />
       <Footer />
 
