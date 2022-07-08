986c42af7c1216381d6beb4cffaabeeaf0f10a9d
StiLKnoT <stilknot@gmail.com>
2022-07-08 12:28:14 +0500

    axios

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,15 +6,40 @@ import tg from '../../assets/images/f-tg.png'
 import inst from '../../assets/images/f-inst.png'
 import yout from '../../assets/images/f-yout.png'
 import fb from '../../assets/images/f-fb.png'
-
+import Axios from 'axios'
 // Icons
 import { RiTelegramLine } from "react-icons/ri";
 import { RiInstagramLine } from "react-icons/ri";
 import { RiYoutubeLine } from "react-icons/ri";
 import { RiFacebookCircleLine } from "react-icons/ri";
+import { useState } from 'react'
 
 
 function Footer() {
+
+    const [data, setData] = useState({
+        fio:"",
+        phone: "",
+        biznes: ""
+    })
+    function handle(e){
+        const newdata={...data}
+        newdata[e.target.id] = e.target.value
+        setData(newdata)
+        console.log(newdata)
+
+    }
+    function submit(e){
+        e.preventDefault();
+        Axios.post("https://62373d82f5f6e28a154abef5.mockapi.io/data",{
+            fio: data.fio,
+            phone: data.phone,
+            biznes: data.biznes
+        })
+        .then(res=>{
+            console.log(res.data)
+        })
+    }
 
     return (
         <div className='Footer' id='Footer'>
@@ -22,15 +47,22 @@ function Footer() {
                 <div className='upperFooter'>
                     <h3 className='partTitle' >Kozimxon Turaev bilan kelajakda o‘z <br /> o‘rniga ega biznesingizni yarating!</h3>
                     <div className='footerForm'>
+                  
                         <div className='footerFormGrid'>
                             <div className='footerItem' >
                                 <p>Ismingizni kiriting</p>
-                                <input className='msgInput ' placeholder='F.I.Sh ...' />
+                                <form onSubmit={(e)=>submit(e)}>
+                                    <input  onChange={(e)=>handle(e)} id="fio" value={data.fio} className='msgInput'  placeholder='F.I.Sh ...' />
+                                </form>
+
                                 <sub className="errorTxt">F.I.Sh notog‘ri kiritilgan!</sub>
                             </div>
                             <div className='footerItem' >
                                 <p>Raqamingizni kiriting</p>
-                                <input className='msgInput ' type="number" placeholder='Raqamingiz ...' />
+                                <form onSubmit={(e)=>submit(e)}>
+                                    <input onChange={(e)=>handle(e)} id="phone" value={data.phone} className='msgInput'  type="number" placeholder='Raqamingiz ...' />
+                                </form>
+
                                 <sub className="errorTxt">Raqamingiz notog‘ri kiritilgan!</sub>
                             </div>
                         </div>
@@ -38,17 +70,23 @@ function Footer() {
                         <div className='footerFormGrid'>
                             <div className='footerItem' >
                                 <p>Biznes sohangizni kiriting</p>
-                                <input className='msgInput ' placeholder='Biznesingiz ...' />
+                                <form onSubmit={(e)=>submit(e)}>
+                                    <input onChange={(e)=>handle(e)} id="biznes" value={data.biznes} className='msgInput'  placeholder='Biznesingiz ...' />
+                                </form>
+
                                 <sub className="errorTxt">Raqamingiz notog‘ri kiritilgan!</sub>
 
                             </div>
                             <div className='footerItem'>
                                 {/* <p></p> */}
+                                <form onSubmit={(e)=>submit(e)}>
                                 <button className='footerbtn' >Ro‘yxatdan o‘tish →</button>
+                                </form>
                                 <sub></sub>
 
                             </div>
                         </div>
+                        
 
                     </div>
                 </div>
